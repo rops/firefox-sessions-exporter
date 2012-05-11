e08337fad12d3b57d391dd198cd78a7f74705eb8
Eros <eros@eros-laptop.(none)>
2012-05-11 18:28:43 +0200

    Save to ZIP added

--- content/overlay.js
+++ content/overlay.js
@@ -14,6 +14,7 @@ function getCookies() {
         dumped.push( cookie );
   }
   //filter cookies and encode in json..
+  return JSON.stringify( dumped );
 };
 
 var SessionManager = {
@@ -38,11 +39,33 @@ var SessionManager = {
   
   receiveMessage: function(aMessage) {
     log("rcv msg from child to parent");
-    this.current_html = aMessage.json.html;
-    this.current_cookie = getCookies();
+//    this.current_html = aMessage.json.html;
+//    this.current_cookie = getCookies();
     //save on disk
-    
+    this.zipToFile( aMessage.json.html, getCookies() );
   },
+  
+	zipToFile : function ( html, cookies ) {
+		var outFile=Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties).get("ProfD", Ci.nsIFile);
+		outFile.append("session.zip");
+		alert( outFile.path );
+		if( ! outFile.exists() )
+			outFile.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0666);
+		var zipWriter = Components.Constructor("@mozilla.org/zipwriter;1", "nsIZipWriter");
+		var zipW = new zipWriter();
+		zipW.open( outFile, 0x04 /*PR_RDWR*/ | 0x08 /*PR_CREATE_FILE*/ | 0x20 /*PR_TRUNCATE*/);
+		var istream = Cc["@mozilla.org/io/string-input-stream;1"].createInstance(Ci.nsIStringInputStream);
+		istream.setData(html, html.length);
+		if (zipW.hasEntry("page.html"))
+		    zipW.removeEntry("page.html",false)
+		zipW.addEntryStream("page.html",null,Ci.nsIZipWriter.COMPRESSION_DEFAULT,istream,false)
+		istream.setData(cookies, cookies.length);
+		if (zipW.hasEntry("cookies.json"))
+		    zipW.removeEntry("cookies.json",false)
+		zipW.addEntryStream("cookies.json",null,Ci.nsIZipWriter.COMPRESSION_DEFAULT,istream,false)
+		zipW.close();
+	},
+
 
   onUIReady : function(aEvent) {
   },
