97a7fbe9b3e8757226cdd82084a4c280a9c11c04
Daniele Rossetti <daniele.rossetti@me.com>
2012-05-28 13:08:29 +0200

    added preferences pane + timer

--- content/overlay.js
+++ content/overlay.js
@@ -1,3 +1,4 @@
+//Services.prefs.getIntPref("extensions.sessionmanager.something");
 function log(msg){
 	let alerts = Cc["@mozilla.org/alerts-service;1"].getService(Ci.nsIAlertsService);
   alerts.showAlertNotification(null, "Log", msg, false, "", null);	
@@ -18,44 +19,55 @@ function getCookies() {
   var cookieSvc = Components.classes["@mozilla.org/cookieService;1"]  
                     .getService(Components.interfaces.nsICookieService);  
   var cookie = cookieSvc.getCookieString(uri, null);
+  //log(cookie);
   return JSON.stringify( cookie );
 
 };
 
 var SessionManager = {
 
+  savingPath:"",
+  savingTimer:0,
   current_html:"",
+  timerID:null,
+  pref:null,
   current_cookie:new Array(),
   onLoad : function(aEvent) {
+    this.initPref();
+    this.timerID = window.setInterval(this.saveSession,this.savingTimer*1000);
+    log("timer: "+this.savingTimer);
     log("loaded");
   },
 
   saveSession: function(){
+    log("timeout");
     //listen for msg from child
-    window.messageManager.addMessageListener("SessMan:ReceiveHTMLFromChild", this);
+    window.messageManager.addMessageListener("SessMan:ReceiveHTMLFromChild", SessionManager);
     //script executed by child process
     window.messageManager.loadFrameScript("chrome://SessionManager/content/content.js", true);
 
-    this.sendMessageToGetHTML();
+    SessionManager.sendMessageToGetHTML();
   },
 
   sendMessageToGetHTML: function() {
     Browser.selectedBrowser.messageManager.sendAsyncMessage("SessMan:AskChildForHTML",{});
-    log("sent from parent to child");
+    //log("sent from parent to child");
   },
   
   receiveMessage: function(aMessage) {
-    log("rcv msg from child to parent");
+    //log("rcv msg from child to parent");
 //    this.current_html = aMessage.json.html;
 //    this.current_cookie = getCookies();
     //save on disk
+    window.messageManager.removeMessageListener("SessMan:ReceiveHTMLFromChild", SessionManager);
     this.zipToFile( aMessage.json.html, getCookies() );
   },
   
 	zipToFile : function ( html, cookies ) {
 		var outFile=Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties).get("ProfD", Ci.nsIFile);
+    //outFile = this.prefs.savingPath;
 		outFile.append("session.zip");
-		alert( outFile.path );
+		//alert( outFile.path );
 		if( ! outFile.exists() )
 			outFile.create(Ci.nsIFile.NORMAL_FILE_TYPE, 0666);
 		var zipWriter = Components.Constructor("@mozilla.org/zipwriter;1", "nsIZipWriter");
@@ -71,8 +83,48 @@ var SessionManager = {
 		    zipW.removeEntry("cookies.json",false)
 		zipW.addEntryStream("cookies.json",null,Ci.nsIZipWriter.COMPRESSION_DEFAULT,istream,false)
 		zipW.close();
+    log("session saved");
 	},
 
+  initPref: function()  
+   {  
+     
+     this.prefs = Components.classes["@mozilla.org/preferences-service;1"]  
+         .getService(Components.interfaces.nsIPrefService)  
+         .getBranch("extensions.sessionmanager.");  
+     this.prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);  
+     this.prefs.addObserver("", this, false);  
+       
+     this.savingPath = this.prefs.getCharPref("savingpath");  
+     this.savingTimer = this.prefs.getIntPref("savingtimer");
+   },
+
+  //observer for preferences changes 
+  observe: function(subject, topic, data) {  
+     if (topic != "nsPref:changed")  
+     {  
+       return;  
+     }  
+   
+     switch(data)  
+     {  
+       case "savingpath":  
+         this.savingPath = this.prefs.getCharPref("savingpath");
+         log(this.savingPath);
+         break;
+       case "savingtimer":  
+         this.savingTimer = this.prefs.getIntPref("savingtimer");
+         window.clearInterval(this.timerID);
+         this.timerID = window.setInterval(this.saveSession,this.savingTimer*1000);
+         log("new timer: "+this.savingTimer);
+         log(this.savingTimer);
+         break;  
+     }  
+  },   
+   
+  shutdown: function() {  
+    this.prefs.removeObserver("", this);  
+   },
 
   onUIReady : function(aEvent) {
   },
@@ -82,6 +134,9 @@ var SessionManager = {
 
 window.addEventListener("load", function(e) {
 	SessionManager.onLoad(e);
+}, false);
+window.addEventListener("unload", function(e) {
+  SessionManager.shutdown(e);
 }, false);
 
 window.addEventListener("UIReady", function(e) {
---
+++ defaults/preferences/defaults.js
@@ -0,0 +1,4 @@
+//var outFile=Cc["@mozilla.org/file/directory_service;1"].getService(Ci.nsIProperties).get("ProfD", Ci.nsIFile);
+pref("extensions.sessionmanager.savingpath", "asd");
+pref("extensions.sessionmanager.savingtimer", 10);
+
